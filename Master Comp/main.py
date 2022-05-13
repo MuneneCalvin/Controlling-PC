820e498dbea690c685a3d66218f3a3277f0c728b
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-05-13 17:55:01 +0300

    Add files via upload

---
+++ Master Comp/main.py
@@ -0,0 +1,18 @@
+import time
+import socket
+import sys
+import os
+
+s = socket.socket()
+host = socket.gethostname()
+port = 8080
+s.bind(('', port))
+s.listen()
+conn, addr = s.accept()
+print(addr, "is connected to server")
+command = input(str("Enter command :"))
+conn.send(command.encode())
+print("Command has been sent successfully.")
+data = conn.recv(1024)
+if data :
+    print("Command received and executed successfully")
